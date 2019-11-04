e684ed66489720beb2d422fa7a5d2489f6d2a53b
penrique <penrique@soflex.com.ar>
2019-11-04 01:59:06 -0300

    using data instead of file

--- projects/ngx-value/src/lib/core/ngx-value.ts
+++ projects/ngx-value/src/lib/core/ngx-value.ts
@@ -1,18 +1,18 @@
 class NgxValue {
 
-    static Value(property: string, file?: string) {
+    static Value(property: string, data?: string) {
         return (target: any, key: string | symbol) => {
 
             let val = undefined;
 
             const getter = () => {
                 if (val === undefined)
-                    val = JSONLoader.getInstance().get(property, file) !== undefined ? JSONLoader.getInstance().get(property, file) : null;
+                    val = JSONLoader.getInstance().get(property, data) !== undefined ? JSONLoader.getInstance().get(property, data) : null;
                 return val;
             };
 
             const setter = (next) => {
-                let value = JSONLoader.getInstance().get(property, file) !== undefined ? JSONLoader.getInstance().get(property, file) : next;
+                let value = JSONLoader.getInstance().get(property, data) !== undefined ? JSONLoader.getInstance().get(property, data) : next;
                 val = value;
             };
 
@@ -25,13 +25,13 @@ class NgxValue {
         };
     }
 
-    static Values(...files: string[]) {
+    static Values(...data: string[]) {
         return () => {
             return () => {
                 return new Promise((resolve, reject) => {
                     let promises = [];
-                    files.forEach(file => {
-                        promises.push(JSONLoader.getInstance().loadJSON(file));
+                    data.forEach(result => {
+                        promises.push(JSONLoader.getInstance().loadJSON(result));
                     });
                     Promise.all(promises).then(x => {
                         resolve(x);
@@ -43,56 +43,56 @@ class NgxValue {
         }
     }
 
-    static Get(property: string, file?: string) {
-        return JSONLoader.getInstance().get(property, file) !== undefined ? JSONLoader.getInstance().get(property, file) : null;
+    static Get(property: string, data?: string) {
+        return JSONLoader.getInstance().get(property, data) !== undefined ? JSONLoader.getInstance().get(property, data) : null;
     }
 }
 
-export function Value(property: string, file?: string) {
-    return NgxValue.Value(property, file);
+export function Value(property: string, data?: string) {
+    return NgxValue.Value(property, data);
 }
 
-export function Values(...files: string[]) {
-    return NgxValue.Values(...files);
+export function Values(...data: string[]) {
+    return NgxValue.Values(...data);
 }
 
-export function Get(property: string, file?: string) {
-    return NgxValue.Get(property, file);
+export function Get(property: string, data?: string) {
+    return NgxValue.Get(property, data);
 }
 
 class JSONLoader {
 
     private static instance: JSONLoader;
-    public promises: { [file: string]: Promise<any>; } = {};
-    public json: { [file: string]: string } = {};
+    public promises: { [data: string]: Promise<any>; } = {};
+    public json: { [data: string]: string } = {};
 
     private constructor() { }
 
-    public get(key: string, file?: string) {
-        file = file != null ? file : 'assets/properties.json';
+    public get(key: string, data?: string) {
+        data = data != null ? data : 'assets/properties.json';
 
-        if (JSONLoader.getInstance().json[file] == null) {
-            console.error(`No file was found for @Value("${key}", "${file}").`);
+        if (JSONLoader.getInstance().json[data] == null) {
+            console.error(`No data was found for @Value("${key}", "${data}").`);
             return;
         }
 
-        if (key === null && JSONLoader.getInstance().json[file] != null && Array.isArray(JSONLoader.getInstance().json[file])) {
-            return JSONLoader.getInstance().json[file];
+        if (key === null && JSONLoader.getInstance().json[data] != null && Array.isArray(JSONLoader.getInstance().json[data])) {
+            return JSONLoader.getInstance().json[data];
         } else if (key === null) {
-            console.error(`No array was found on "${file}".`);
-        } else if (JSONLoader.getInstance().json[file] != null && JSONLoader.getInstance().json[file][key] !== undefined) {
-            return JSONLoader.getInstance().json[file][key];
+            console.error(`No array was found on "${data}".`);
+        } else if (JSONLoader.getInstance().json[data] != null && JSONLoader.getInstance().json[data][key] !== undefined) {
+            return JSONLoader.getInstance().json[data][key];
         } else {
-            console.error(`Property "${key}" not found on "${file}".`);
+            console.error(`Property "${key}" not found on "${data}".`);
         }
     }
 
-    public loadJSON(file: string) {
+    public loadJSON(data: string) {
         return new Promise((resolve, reject) => {
-            file = file != null ? file : 'assets/properties.json';
+            data = data != null ? data : 'assets/properties.json';
 
-            if (JSONLoader.getInstance().promises[file] != undefined) {
-                return JSONLoader.getInstance().promises[file].then((value) => {
+            if (JSONLoader.getInstance().promises[data] != undefined) {
+                return JSONLoader.getInstance().promises[data].then((value) => {
                     resolve(value);
                 }).catch((reason) => {
                     reject(reason);
@@ -101,24 +101,24 @@ class JSONLoader {
             else {
                 let xhr = new XMLHttpRequest();
                 xhr.overrideMimeType("application/json");
-                xhr.open('GET', file, true);
+                xhr.open('GET', data, true);
                 xhr.onload = function () {
                     if (xhr.readyState == 4 && xhr.status == 200) {
                         try {
-                            JSONLoader.getInstance().json[file] = JSON.parse(xhr.responseText);
-                            resolve(JSONLoader.getInstance().json[file]);
+                            JSONLoader.getInstance().json[data] = JSON.parse(xhr.responseText);
+                            resolve(JSONLoader.getInstance().json[data]);
                         } catch (e) {
-                            console.error(`Failed to parse JSON file! Please verify "${file}".`);
+                            console.error(`Failed to parse JSON data! Please verify "${data}".`);
                             resolve({});
                         }
                     }
                     else {
-                        console.error(`File not found! ("${file}")`);
+                        console.error(`data not found! ("${data}")`);
                         resolve({});
                     }
                 };
                 xhr.onerror = () => {
-                    console.error(`Failed to load "${file}"`);
+                    console.error(`Failed to load "${data}"`);
                     resolve({});
                 }
                 xhr.onabort = xhr.onerror;
